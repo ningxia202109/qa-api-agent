8b00925aa2f3a6a088373ee00e14932ce4bc7579
ningxia202109 <90520833+ningxia202109@users.noreply.github.com>
2024-12-09 14:58:29 -0800

    update (#2)

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -1,7 +1,9 @@
 from .CoderAgent import CoderAgent
 from .swagger_agent import swagger_agent
+from .code_writer_agent import code_writer_agent
 
 __all__ = [
     "CoderAgent",
     "swagger_agent",
+    "system_prompt",
 ]
